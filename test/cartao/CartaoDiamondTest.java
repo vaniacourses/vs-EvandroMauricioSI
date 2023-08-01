a7f12bfd26122ebb0145c881ed9702cbab43b97c
Evandro Mauricio <evandromauricio@id.uff.br>
2023-07-31 22:13:48 -0300

    new commit

--- test/cartao/CartaoDiamondTest.java
+++ test/cartao/CartaoDiamondTest.java
@@ -1,5 +1,23 @@
 package cartao;
 
+import org.junit.jupiter.api.Test;
+
 public class CartaoDiamondTest {
+	Cartao cartaoDiamondClass;
+	
+	@Test
+    public void testConstrutor() {
+        assertEquals("João Silva", cartaoDiamondClass.getNomeTitular());
+        assertEquals("1234567890123456", cartaoDiamondClass.getNumeroCartao());
+        assertEquals("123", cartaoDiamondClass.getCvc());
+        assertEquals("12/2023", cartaoDiamondClasss.getValidade().formatado());
+        assertEquals(VerificadorEntrada.DIAMOND, cartaoDiamondClass.getTipoCartao());
+    }
+	
+	@Test
+	public void testGetLimiteMaximo() {
+	    double limiteEsperado = 90000.0;
+	    double limiteObtido = cartaoDiamondClass.getLimiteMaximo();
+	    assertEquals(limiteEsperado, limiteObtido);
 
 }
--- test/cartao/CartaoTest.java
+++ test/cartao/CartaoTest.java
@@ -1,6 +1,21 @@
 package cartao;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+@DisplayName ("Classe para teste do cartão")
 public class CartaoTest {
+	Cartao cartaoClass;
 
-
+	@Test
+	public void testToString() {
+	    String stringEsperada = "[CARTAO DE CREDITO]\nAPELIDO: João Silva\n"
+	    		+ "NUMERO DO CARTAO: 1234567890123456\nCVC: 123\nVALIDADE: 12/2023\nIDENTIFICACAO: João Silva";
+	    assertEquals(stringEsperada, cartaoClass.toString());
+	}
 }
+
+
